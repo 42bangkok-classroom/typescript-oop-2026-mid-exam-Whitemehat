d66a3473eb933f0190fb0511f344ead51c7e7d0f
Whitemehat <wong1234.pe@gmail.com>
2026-01-21 15:33:47 +0700

    10th

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -16,6 +16,3 @@ function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
-
-let result : number[] = getUniqueNumbers(arr1,arr2);
-console.log(result);
