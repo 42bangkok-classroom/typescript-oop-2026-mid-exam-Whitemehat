b2c6dadf5b387f631c5c19775071854a25f12efd
Whitemehat <wong1234.pe@gmail.com>
2026-01-21 15:32:27 +0700

    9th

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -2,15 +2,13 @@ function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
   let arr_r : number[] = [];
   for(const n1 of arr1){
-    arr_r.push(n1);
-  }
-  for(const i in arr2){
-    if(!arr_r.includes(arr2[i]))
-    {
-      arr_r.push(arr2[i]);
+    if(!arr2.includes(n1)){
+      arr_r.push(n1);
     }
-    else{
-      
+  }
+  for(const n2 of arr2){
+    if(!arr1.includes(n2)){
+      arr_r.push(n2);
     }
   }
   return arr_r;
