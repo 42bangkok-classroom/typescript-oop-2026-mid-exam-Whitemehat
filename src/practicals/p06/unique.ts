85ca3fe8ef1f94ac2d58a268ea6cbeee203995ee
Whitemehat <wong1234.pe@gmail.com>
2026-01-21 15:42:26 +0700

    13th

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,4 +1,4 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
   let arr_r : number[] = [];
   for(const n1 of arr1){
@@ -18,3 +18,9 @@ function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
     return arr_r;
   }
 }
+
+const arr1 = [1, 2, 3, 4];
+const arr2 = [3, 4, 5, 6];
+
+//const result = getUniqueNumbers(arr1,arr2);
+//console.log(result);
